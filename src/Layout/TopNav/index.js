3e1a352739a4664da619bcc14e4df7eff9f3d7dd
Oluwatosin Olasupo <oluwateeblaq@gmail.com>
2022-04-23 10:42:11 +0100

    fixed changes

--- src/Layout/TopNav/index.js
+++ src/Layout/TopNav/index.js
@@ -126,8 +126,13 @@ const TopNav = () => {
     )
   }
   return (
-    <Container>
-      <div className="content--container nav--desktop">
+    <Container style={{flexDirection: "column", paddingBottom: "9rem"}}>
+      <div style={{backgroundColor: "#051A26", width: "100%", height: "auto", color: "white", textAlign: "center", padding: "15px 5px", fontSize: "18px"}}>
+        <a href='https://tally.so/r/w7QBPm?transparentBackground=1' target={"_blank"} style={{color: "inherit", display: "block", width: "100%", height: "100%"}}>
+        Register for our <span style={{textDecoration: "underline"}}>Career Starter Program</span> for <span style={{textDecoration: "underline"}}>University Students in Nigeria</span>. With up to <span style={{textDecoration: "underline"}}>60%</span> off our normal prices <img style={{paddingTop: "7.5px"}}></img>
+        </a>
+      </div>
+      <div className="content--container nav--desktop" style={{padding: "20px 0", background: "white"}}>
         <Link to="/" className="brand--logo">
           <AppLogo />
         </Link>
--- src/base/theme.js
+++ src/base/theme.js
@@ -1,8 +1,8 @@
 import mixins from './mixins'
 
 const dimensions = {
-  navHeight: '6rem',
-  navHeightMobile: '4.5rem',
+  navHeight: '7.5rem',
+  navHeightMobile: '6rem',
   maxWidth: '1100px',
   adminTopNavHeight: '4.5rem',
   footerHeight: '3.5rem',
